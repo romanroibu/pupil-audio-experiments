028cd729677f2d4219dbcd1335d1b61824b1962d
Roman Roibu <romanroibu@gmail.com>
2019-11-27 14:17:05 +0100

    Update examples/_pupil_audio.py to re-export from blocking submodule

--- examples/_pupil_audio.py
+++ examples/_pupil_audio.py
@@ -5,9 +5,9 @@ import pathlib
 
 # Re-export pupil_audio API
 sys.path.insert(0, str(pathlib.Path(__file__).parent.parent.absolute()))
-from pupil_audio import *
-from pupil_audio import base
-from pupil_audio import wave
-from pupil_audio import pyav
-from pupil_audio import pyaudio
+from pupil_audio.blocking import *
+from pupil_audio.blocking import base
+from pupil_audio.blocking import wave
+from pupil_audio.blocking import pyav
+from pupil_audio.blocking import pyaudio
 del sys.path[0]
