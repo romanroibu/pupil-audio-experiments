aebcbb31505a8bca24ba05a862c25dc6fb233da5
Roman Roibu <romanroibu@gmail.com>
2019-11-28 14:11:00 +0100

    Update nonblocking pyaudio2pyav to use an optional fixed recording length

--- examples/capture_nonblocking_pyaudio2pyav.py
+++ examples/capture_nonblocking_pyaudio2pyav.py
@@ -2,10 +2,12 @@ def main(
     in_name,
     out_path,
     frame_rate=None,
+    duration=None,
 ):
     import time
     from pupil_audio.nonblocking import PyAudio2PyAVCapture
 
+    duration = duration or float("inf")
 
     capture = PyAudio2PyAVCapture(
         in_name=in_name,
@@ -13,9 +15,11 @@ def main(
         frame_rate=frame_rate,
     )
 
+    start_time = time.monotonic()
+
     try:
         capture.start()
-        while True:
+        while time.monotonic() - start_time < duration:
             time.sleep(0.1)
     except KeyboardInterrupt:
         pass
@@ -29,18 +33,22 @@ if __name__ == "__main__":
 
     @click.command()
     @click.option("--frame_rate", default=None, type=click.INT, help="Frame rate used for the input and output (if not set, the default input frame rate is used)")
-    def cli(frame_rate):
+    @click.option("--duration", default=None, type=click.FLOAT, help="Duration of the recording (if not set, the user should manually stop the recording with Ctrl+C)")
+    def cli(frame_rate, duration):
+        duration_str = f"{duration}_sec" if duration else None
         in_name = example_utils.get_user_selected_input_name()
         out_path = example_utils.get_output_file_path(
             __file__,
             in_name,
             frame_rate,
+            duration_str,
             ext="mp4"
         )
         main(
             in_name=in_name,
             out_path=out_path,
             frame_rate=frame_rate,
+            duration=duration,
         )
 
     cli()
