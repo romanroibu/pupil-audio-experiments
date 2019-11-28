16c5c22992dde5611613e2aca4b998200a94efab
Roman Roibu <romanroibu@gmail.com>
2019-11-28 14:08:20 +0100

    Update nonblocking pyaudio2pyav example to use click for cli

--- examples/capture_nonblocking_pyaudio2pyav.py
+++ examples/capture_nonblocking_pyaudio2pyav.py
@@ -4,11 +4,8 @@ def main(
     frame_rate=None,
 ):
     import time
-    import examples.utils as example_utils
     from pupil_audio.nonblocking import PyAudio2PyAVCapture
 
-    in_name = example_utils.get_user_selected_input_name()
-    out_path = example_utils.get_output_file_path(__file__, in_name, ext="mp4")
 
     capture = PyAudio2PyAVCapture(
         in_name=in_name,
@@ -27,4 +24,23 @@ def main(
 
 
 if __name__ == "__main__":
-    main()
+    import click
+    import examples.utils as example_utils
+
+    @click.command()
+    @click.option("--frame_rate", default=None, type=click.INT, help="Frame rate used for the input and output (if not set, the default input frame rate is used)")
+    def cli(frame_rate):
+        in_name = example_utils.get_user_selected_input_name()
+        out_path = example_utils.get_output_file_path(
+            __file__,
+            in_name,
+            frame_rate,
+            ext="mp4"
+        )
+        main(
+            in_name=in_name,
+            out_path=out_path,
+            frame_rate=frame_rate,
+        )
+
+    cli()
