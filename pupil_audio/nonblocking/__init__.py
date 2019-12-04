97a246501696d08ff91afa7ece91cecf5c071ba0
Roman Roibu <romanroibu@gmail.com>
2019-12-04 15:57:26 +0100

    Add implementation of PyAudioDeviceMonitor and PyAudioBackgroundDeviceMonitor

--- pupil_audio/nonblocking/__init__.py
+++ pupil_audio/nonblocking/__init__.py
@@ -1,3 +1,3 @@
 from .pyav import PyAVFileSink
-from .pyaudio import PyAudioDeviceSource
+from .pyaudio import PyAudioDeviceSource, PyAudioDeviceMonitor, PyAudioBackgroundDeviceMonitor
 from .pyaudio2pyav import PyAudio2PyAVCapture, PyAudio2PyAVTranscoder
