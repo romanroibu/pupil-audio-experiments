9ced641941f7c7ef05c987a638e95b75f8dd63a8
Roman Roibu <romanroibu@gmail.com>
2019-12-12 14:50:43 +0100

    Self the classes of transcoder, source and sink in PyAudio2PyAVCapture

--- pupil_audio/nonblocking/pyaudio2pyav.py
+++ pupil_audio/nonblocking/pyaudio2pyav.py
@@ -36,20 +36,20 @@ class PyAudio2PyAVCapture:
 
         self.shared_queue = queue.Queue()
 
-        source_cls = source_cls or PyAudioDeviceSource
-        assert issubclass(source_cls, PyAudioDeviceSource)
+        self.source_cls = source_cls or PyAudioDeviceSource
+        assert issubclass(self.source_cls, PyAudioDeviceSource)
 
-        transcoder_cls = transcoder_cls or PyAudio2PyAVTranscoder
-        assert issubclass(transcoder_cls, PyAudio2PyAVTranscoder)
+        self.transcoder_cls = transcoder_cls or PyAudio2PyAVTranscoder
+        assert issubclass(self.transcoder_cls, PyAudio2PyAVTranscoder)
 
-        sink_cls = sink_cls or PyAVFileSink
-        assert issubclass(sink_cls, PyAVFileSink)
+        self.sink_cls = sink_cls or PyAVFileSink
+        assert issubclass(self.sink_cls, PyAVFileSink)
 
-        self.transcoder = transcoder_cls(
+        self.transcoder = self.transcoder_cls(
             frame_rate=frame_rate, channels=channels, dtype=dtype,
         )
 
-        self.source = source_cls(
+        self.source = self.source_cls(
             device_index=device.index,
             frame_rate=self.transcoder.frame_rate,
             channels=self.transcoder.channels,
@@ -57,7 +57,7 @@ class PyAudio2PyAVCapture:
             out_queue=self.shared_queue,
         )
 
-        self.sink = sink_cls(
+        self.sink = self.sink_cls(
             file_path=out_path,
             transcoder=self.transcoder,
             in_queue=self.shared_queue,
